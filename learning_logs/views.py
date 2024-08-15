b1c99f97d22083887176f379bb9cd1452d4c9ef5
Eujin Blank <eujinnicki@gmail.com>
2024-08-15 06:55:11 +0200

    No more data breaches, for every acces to each entry we first check if the topic which the entry belongs to belongs to the user

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -23,8 +23,7 @@ def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
     # Make sure the topic belongs to the current user
-    if topic.owner != request.user:
-        raise Http404
+    check_topic_owner(request,topic)
     
     
     entries = topic.entry_set.order_by("-date_added")
@@ -55,6 +54,7 @@ def new_topic(request):
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic"""
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(request, topic)
     
     if request.method != "POST":
         # No data submitted: create a blank form.
@@ -79,6 +79,7 @@ def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    check_topic_owner(request,topic)
     
     if request.method != "POST":
         # Initial request; pre-fill form with current entry.
@@ -93,4 +94,9 @@ def edit_entry(request, entry_id):
         
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, "learning_logs/edit_entry.html", context)
+
+def check_topic_owner(request,topic):
+    """Check if the the topic being accessed belongs to the current user"""
+    if topic.owner != request.user:
+        raise Http404
     
